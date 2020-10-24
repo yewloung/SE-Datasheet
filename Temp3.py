23e26c4e91949cfb4e3ed7f7319a2f8eb87213b5
YewLoung <yewloung@gmail.com>
2020-10-24 20:08:11 +0800

    Refactor program structure into functions and main routines

--- Temp3.py
+++ Temp3.py
@@ -1,16 +1,13 @@
-import sys
-import keyboard
+import thread
 
-a=[1,2,3,4]
-print("Press Enter to continue or press Esc to exit: ")
-while True:
-    try:
-        if keyboard.is_pressed('ENTER'):
-            print("you pressed Enter, so printing the list..")
-            print(a)
-            break
-        if keyboard.is_pressed('Esc'):
-            print("\nyou pressed Esc, so exiting...")
-            sys.exit(0)
-    except:
-        break
+def input_thread(a_list):
+    raw_input()
+    a_list.append(True)
+
+def do_stuff():
+    a_list = []
+    thread.start_new_thread(input_thread, (a_list,))
+    while not a_list:
+        stuff()
+
+
