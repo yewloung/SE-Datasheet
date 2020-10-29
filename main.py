f1e08b270ea0024aedea9cdd0e738817c3355771
YewLoung <yewloung@gmail.com>
2020-10-30 07:36:22 +0800

    1) Combine product spec in specified excel format into datasheet for comparison

--- main.py
+++ main.py
@@ -24,6 +24,8 @@ ave_total_time = 0
 ave_time_left = 0
 input_file = r'ref.xlsx'
 output_file = r'SE_web_datasheet.xlsx'
+spec_file = r'SE_spec.xlsx'
+spec_worksheet = 'spec2'
 
 # function to deal with excel formating
 def autosize_excel_columns_df(worksheet, df, offset=0):
@@ -63,6 +65,7 @@ def get_web_datasheet(url):
         reference = soup.find('div', {'data-autotests-id': 'product-id'})
         reference = reference.next
         reference = reference.string.replace(" ", "")
+        reference = reference.strip()
 
         htmltables = soup.find_all('table', {'class': 'pes-table'})
         for htmltable in htmltables:
@@ -107,10 +110,18 @@ def main():
     global ave_execution_time
     global ave_total_time
     global ave_time_left
+    global input_file
+    global output_file
+    global spec_file
+    global spec_worksheet
 
-    if os.path.isfile(input_file) == True:
+    if (os.path.isfile(input_file) == True):
         # read ref.xlsx, expect reference name must be put at col 1
         ref_df = pd.read_excel(input_file, header=None)
+
+        if (os.path.isfile(spec_file) == True):
+            # read SE_spec.xlsx
+            spec_df = pd.read_excel(spec_file, sheet_name=spec_worksheet)
 
         if (ref_df.empty != True):
             ref_df[1] = 'https://www.se.com/ww/en/product/' + ref_df[0] + '/'  # create url for each references @ 2nd col
@@ -149,18 +160,30 @@ def main():
             # add param id information
             df['Param_ID'] = df['Reference'] + df['Parameters']
 
-            df['Spec_ID'] = df['Param_ID']
-            df['Spec_Data'] = df['Value'] + 'XXXX'
+            if (os.path.isfile(spec_file) == True):
+                # map spec data
+                # df['Spec_ID'] = df['Param_ID'].map(spec_df.set_index('Param_ID')['Param_ID'])
+                df['Spec_Data'] = df['Param_ID'].map(spec_df.set_index('Param_ID')['Value']).astype(str)
+
+            #df['Spec_ID'] = df['Param_ID']
+            #df['Spec_Data'] = df['Value'] + 'XXXX'
 
             # put the status of scraped reference in col 3
             ref_df[2] = status
 
             # generate pivot table, put 'Value' for each 'Reference' arranged into column
-            pivot = df.pivot_table(index=['Section', 'Parameters'],
-                                   columns=['Reference'],
-                                   values=['Value', 'Spec_Data'],
-                                   aggfunc={'Value': lambda x: ' '.join(x),
-                                            'Spec_Data': lambda x: ' '.join(x)})
+            if (os.path.isfile(spec_file) == True):
+                pivot = df.pivot_table(index=['Section', 'Parameters'],
+                                       columns=['Reference'],
+                                       values=['Value', 'Spec_Data'],
+                                       aggfunc={'Value': lambda x: ' '.join(x),
+                                                'Spec_Data': lambda x: ' '.join(x)})
+            else:
+                pivot = df.pivot_table(index=['Section', 'Parameters'],
+                                       columns=['Reference'],
+                                       values=['Value'],
+                                       aggfunc={'Value': lambda x: ' '.join(x)})
+
             pivot = pivot.swaplevel(0, 1, axis=1).sort_index(axis=1)  # swap column levels
 
             pivot1 = df.pivot_table(index=['Section', 'Parameters'],
@@ -177,7 +200,7 @@ def main():
             #                        aggfunc='count',
             #                        fill_value=0)
 
-            print(pivot1)
+            #print(pivot1)
 
             ''' --------------------  export data frame to excel operation   -------------------- '''
             writer = pd.ExcelWriter(output_file, engine='xlsxwriter')  # associated panda to xlsxwriter engine
@@ -188,11 +211,15 @@ def main():
             # export df data frame to "raw_datasheet" worksheet
             df.to_excel(writer, index=True, header=True, sheet_name='raw_datasheet')
 
-            # export df data frame to "pivot_datasheet" worksheet
+            # export pivot data frame to "pivot_datasheet" worksheet
             pivot.to_excel(writer, index=True, header=True, sheet_name='pivot_datasheet')
 
-            # export df data frame to "pivot1_datasheet" worksheet
+            # export pivot1 data frame to "pivot1_datasheet" worksheet
             pivot1.to_excel(writer, index=True, header=True, sheet_name='pivot1_datasheet')
+
+            if (os.path.isfile(spec_file) == True):
+                # export spec_df data frame to "spec" worksheet
+                spec_df.to_excel(writer, index=False, header=True, sheet_name='spec')
 
             # update status of scraped status back to input_file
             #ref_df.to_excel(input_file, index=False, header=None)
@@ -209,6 +236,7 @@ def main():
             # assign worksheet "status" variable name as "status_worksheet"
             status_worksheet = writer.sheets['status']
             autosize_excel_columns(status_worksheet, ref_df)
+            status_worksheet.set_zoom(80)
 
             # assign worksheet "raw_datasheet" variable name as "df_worksheet"
             df_worksheet = writer.sheets['raw_datasheet']
@@ -220,6 +248,7 @@ def main():
             df_worksheet.set_column(first_col=6, last_col=6, width=40, cell_format=text_align_format)
             df_worksheet.set_column(first_col=7, last_col=7, width=95, cell_format=text_align_format)
             df_worksheet.freeze_panes(1, 0)
+            df_worksheet.set_zoom(80)
 
             # assign worksheet "pivot_datasheet" variable name as "pivot_worksheet"
             pivot_worksheet = writer.sheets['pivot_datasheet']
@@ -228,6 +257,7 @@ def main():
             #autosize_excel_columns(pivot_worksheet, pivot)
             pivot_worksheet.set_column(first_col=2, last_col=(found_count + 1)*2, width=50, cell_format=text_align_format)
             pivot_worksheet.freeze_panes(3, 2)
+            pivot_worksheet.set_zoom(80)
 
             # assign worksheet "pivot1_datasheet" variable name as "pivot1_worksheet"
             pivot1_worksheet = writer.sheets['pivot1_datasheet']
@@ -236,11 +266,26 @@ def main():
             #autosize_excel_columns(pivot1_worksheet, pivot1)
             pivot1_worksheet.set_column(first_col=2, last_col=found_count + 1, width=110, cell_format=text_align_format)
             pivot1_worksheet.freeze_panes(1, 2)
+            pivot1_worksheet.set_zoom(80)
+
+            if (os.path.isfile(spec_file) == True):
+                # assign worksheet "spec" variable name as "spec_worksheet"
+                spec_worksheet = writer.sheets['spec']
+                spec_worksheet.set_column('A:A', 50, text_align_format)
+                spec_worksheet.set_column('B:B', 150, text_align_format)
+                # autosize_excel_columns(spec_worksheet, spec_df)
+                spec_worksheet.freeze_panes(1, 1)
+                spec_worksheet.set_zoom(80)
 
             writer.save()
 
+            ''' --------------------  read back worksheets created   -------------------- '''
+            xls = pd.ExcelFile(output_file)
+
             # get total program execution time
-            print('\n--- Total program run time is %s seconds ---' % round(time.time() - start_time, 2))
+            print('\n')
+            print('---', xls.sheet_names, 'worksheets are created into', output_file, ' ---')
+            print('--- Total program run time is %s seconds ---' % round(time.time() - start_time, 2))
             print('--- Total / found / not found :', len(urls), '/', found_count, '/', notfound_count, ' ---')
 
         else:
